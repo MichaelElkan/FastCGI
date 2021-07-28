4490cb02c19f9e7cc46e9d601604813cf010778c
MichaelElkan <melkan22@gmail.com>
2021-07-28 15:49:42 -0400

    added comments fixed docker file

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ ARG count
 ARG socket
 ARG queue-size
 
-RUN python3 fcgi-pm.py --socket=${socket} --count=${count} --queue-size=${queue-size} --executable=${executable}
+CMD python3 fcgi-pm.py --socket=${socket} --count=${count} --queue-size=${queue-size} --executable=${executable}
 
--- fcgi-pm.py
+++ fcgi-pm.py
@@ -1,10 +1,7 @@
-#initial commit
-#todo: 
-# docker
-# bind to port
-# exit clean up
-# variable logging verbosity
+# Michael Elkan
+# 7/28/2021
 
+# standard library imports
 import argparse
 import multiprocessing
 import subprocess
@@ -15,6 +12,7 @@ import socket
 import sys
 from datetime import datetime
 
+# command line argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-s','--socket', help='Specify the socket.')
 parser.add_argument('-q','--queue-size', help='Specify the queue size.')
@@ -23,16 +21,18 @@ parser.add_argument('-e', '--executable', help='Specify the executable and any o
 parser.add_argument('-v', '--verbose', action='store_true', help='Specify if you want verbose output.')
 args = parser.parse_args()
 
+# command line arguments
 SOCKET_ARG = args.socket
 QUEUESIZE = int(args.queue_size)
 COUNT = int(args.count)
 EXECUTABLE = args.executable
 VERBOSE = args.verbose
-print(args)
+# Process list for clean up
 PROCESS_LIST = []
 
+
 def main():
-    bindport()
+    SOCKET = bindport() 
     if VERBOSE:
         log("Starting Fcgi-pm at " + str(datetime.now()))
     pool = multiprocessing.Pool(processes=COUNT)
@@ -42,26 +42,38 @@ def main():
     r.wait()
     print(results)
 
+# binds socket and waits for connection
 def bindport():
+    # get host and port variables from command line argument
     HOST = SOCKET_ARG.split(':')[0]
     PORT = int(SOCKET_ARG.split(':')[1])
+    
+    # initialize and bind socket
     SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         SOCKET.bind((HOST, PORT))
     except socket.error as msg:
         print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
-    print('Socket bind complete')
+    if VERBOSE:
+        log("Socket succesfully bound: " + str(SOCKET) + " at " + str(datetime.now()) )
+    # waits for connection to be made
+    SOCKET.listen(5)
+    cli, addr = SOCKET.accept()
 
+# main driver of fcgi executables
 def work(value):
+    # spawns a child subprocess 
     process = subprocess.Popen(EXECUTABLE, shell=True, stdin=subprocess.PIPE)
-  
+    
+    # saves the process ID to use for clean up if needed
     PROCESS_ID = process.pid
+    PROCESS_LIST.append(PROCESS_ID)
+    # logging if desired
     if VERBOSE:
         log("Starting process " + str(PROCESS_ID) + " at " + str(datetime.now()))
 
-    PROCESS_LIST.append(PROCESS_ID)
+    # passes socket to fcgi executable through stdin
     process.communicate(bytes(SOCKET_ARG, encoding='utf-8'))
     process.wait()
    
@@ -71,13 +83,14 @@ def work(value):
     PROCESS_LIST.remove(PROCESS_ID)
     return process
 
-
+# clean up if any process are still running at program exit
 def cleanup():
     for pid in PROCESS_LIST:
         if pid != None:
             print("Killing: ", pid)
             os.kill(pid, signal.SIGTERM)
 
+# logging function
 def log(text):
     try:
         FILE = open('logfile.txt', 'a')
@@ -91,7 +104,5 @@ def log(text):
     
 
 atexit.register(cleanup)
-#signal.signal(signal.SIGTERM, cleanup)
-#signal.signal(signal.SIGINT, cleanup)
 
 main()
